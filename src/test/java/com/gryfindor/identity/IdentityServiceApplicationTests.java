0d62fd4aed816da2a0e7a37b0f4d28e0d548d2b0
mprasanga <masithprasanga@gmail.com>
2025-05-03 20:28:43 +0530

    identity service created

---
+++ src/test/java/com/gryfindor/identity/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gryfindor.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
